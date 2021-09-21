c69a21e745212c9d23e5559d174e56149ad3a999
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-09-21 16:03:26 +0200

    Correction Jeu

--- JavaScript-API-Web/ex-jeu.js
+++ JavaScript-API-Web/ex-jeu.js
@@ -9,20 +9,60 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà saisi : ' + essais.join(', '));
+  }
+
   // pose la question tout de suite mais ne bloque pas le thread pour
   // obtenir la réponse (async)
   rl.question('Quel est le nombre entier entre 0 et 100 ? ', (answer) => {
     console.log('Vous avez saisi : ' + answer);
 
-    // rejouer
-    jouer();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // quitter (si trouvé)
-    // rl.close();
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Il faut saisir un entier');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
 
 // ^
