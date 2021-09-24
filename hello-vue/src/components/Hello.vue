3df57a6d586deb7de113019450cea8277cd089f8
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-09-24 11:33:27 +0200

    hello-vue

---
+++ hello-vue/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    Hello {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ hello-vue/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Hello name="Romain"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hello from '@/components/Hello.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Hello
+  }
+}
+</script>
